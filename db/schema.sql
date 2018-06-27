c93eda18ec46f02646ea972d4785ff9719e95a2c
Alex Vargas <alexv2489@gmail.com>
2018-06-27 12:02:25 -0400

    Create db

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE instagramlite;
+
+\c instagramlite
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  photo_url TEXT,
+  description TEXT,
+  published_at TIMESTAMP
+);
---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+\c instagramlite
+
+DELETE FROM users;
+DELETE FROM posts;
+
+INSERT INTO users (name) VALUES ('Alex');
+INSERT INTO users (name) VALUES ('Jenny');
+INSERT INTO users (name) VALUES ('David');
+INSERT INTO users (name) VALUES ('Felix');
+INSERT INTO users (name) VALUES ('Diana');
