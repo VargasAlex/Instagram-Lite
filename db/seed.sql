2d0c3fa9adf9ed5a0b26fcbddfbcf7e1566b0c9d
Alex Vargas <alexv2489@gmail.com>
2018-09-10 15:59:44 -0400

    Update user profile pictures in database

--- db/seed.sql
+++ db/seed.sql
@@ -4,7 +4,7 @@ DELETE FROM posts;
 DELETE FROM users;
 
 INSERT INTO users (name, img_url)
-VALUES ('AlexTrebek', 'https://cdn.vox-cdn.com/thumbor/lyT5R-zBSQYmbEU3VuNMReGBYjg=/139x0:2360x1666/1820x1213/filters:focal(139x0:2360x1666):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/55901233/1_DjIgGy5umBkyltPq77WFdg.0.jpeg');
+VALUES ('AlexTrebek', 'http://www.saturdayeveningpost.com/wp-content/uploads/satevepost/pg31-jf2016-alex-trebek-slider.jpg');
 
 INSERT INTO users (name, img_url)
 VALUES ('JimHalpert', 'https://fandomitis.files.wordpress.com/2016/09/img_3440.png');
